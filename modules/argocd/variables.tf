a7b21ec7bfe1269deb7dae42cc99fcc1730e3156
Thomas Schuetz <thomas.schuetz@t-sc.eu>
2025-06-29 17:07:48 +0200

    feat: update token variables
    Signed-off-by: Thomas Schuetz <thomas.schuetz@t-sc.eu>

--- modules/argocd/variables.tf
+++ modules/argocd/variables.tf
@@ -11,7 +11,7 @@ variable "repo_secret" {
     username = string
     token    = string
   })
-  default = {}
+  default = null
 }
 
 variable "deploy_key" {
@@ -21,7 +21,7 @@ variable "deploy_key" {
     repo = string
     key  = string
   })
-  default = {}
+  default = null
 }
 
 variable "projects" {
