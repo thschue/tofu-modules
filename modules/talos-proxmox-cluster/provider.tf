89a62c3151ca5f9529191e05eccf7d586646cd36
Thomas Schuetz <thomas.schuetz@t-sc.eu>
2026-03-07 09:46:02 +0100

    chore: update versions
    Signed-off-by: Thomas Schuetz <thomas.schuetz@t-sc.eu>

--- modules/talos-proxmox-cluster/provider.tf
+++ modules/talos-proxmox-cluster/provider.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     proxmox = {
       source  = "bpg/proxmox"
-      version = "0.70.1"
+      version = "0.98.0"
     }
     talos = {
       source  = "siderolabs/talos"
-      version = "0.7.1"
+      version = "0.10.1"
     }
   }
 }
