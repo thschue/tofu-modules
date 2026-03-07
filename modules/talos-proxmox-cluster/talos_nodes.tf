70e0a30248b0b9df31e9f5cb16d3f69bffd46154
Thomas Schuetz <thomas.schuetz@t-sc.eu>
2026-03-07 10:52:17 +0100

    chore: fix addressing
    Signed-off-by: Thomas Schuetz <thomas.schuetz@t-sc.eu>

--- modules/talos-proxmox-cluster/talos_nodes.tf
+++ modules/talos-proxmox-cluster/talos_nodes.tf
@@ -22,7 +22,7 @@ data "talos_machine_configuration" "node" {
           interfaces = [
             # see https://www.talos.dev/v1.8/talos-guides/network/vip/
             {
-              interface = "eth"
+              interface = "eth0"
               address = "${cidrhost(local.node_network, count.index + 3)}/${var.talos_network.subnet_cidr}"
             },
             {
